af9048d2a9db29fb1addda862ff14ec899319bd4
Dara Yve <darayve@gmail.com>
2021-06-25 19:48:09 -0300

    componente de contato com foto, apelido e ícone

--- lib/components/contact_tile.dart
+++ lib/components/contact_tile.dart
@@ -1,25 +1,69 @@
 import 'package:flutter/material.dart';
 
-import 'package:contatissimos/utils/ContactsHandler.dart';
+import 'package:contatissimos/utils/constants.dart' as constants;
 
 class ContactTile extends StatelessWidget {
-  // Método construtor
-  // Variáveis: leading, trailing, title
+  ContactTile({this.contactPhoto, this.apelido, this.onTap});
 
-  ContactTile({this.leading, this.trailing, this.title});
-
-  final Icon leading;
-  final Icon trailing;
-  final String title;
+  final CircleAvatar contactPhoto;
+  final String apelido;
+  final Function onTap;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: alterar estrutura de contato
-    return ListTile(
-      leading: leading,
-      trailing: trailing,
-      title: Text(title),
-      onTap: ContactsHandler.seeContactInfo,
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(1, 1),
+                  blurRadius: 3,
+                )
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: onTap,
+                    behavior: HitTestBehavior.translucent,
+                    child: Row(
+                      children: [
+                        contactPhoto,
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          apelido,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    print('Enable phone call');
+                  },
+                  child: Icon(
+                    Icons.phone,
+                    color: constants.secondary,
+                    size: 30,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 13,
+        )
+      ],
     );
   }
 }
