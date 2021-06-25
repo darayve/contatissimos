85929da3405c5b0799e498a3555382376bb7b171
Dara Yve <darayve@gmail.com>
2021-06-25 16:11:26 -0300

    atualização nas strings

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,3 +1,10 @@
+// Loading screen
+const loadingText = 'CARREGANDO';
+const somethingWentWrong = 'ERRO!';
+
+// Firebase database
+const collectionName = 'contatos';
+
 // App name
 const appDisplayName = 'Contatíssimos';
 const appNewContactTitle = 'Novo contato';
@@ -8,6 +15,11 @@ const searchButtonTooltip = 'Procurar contato';
 // About buttons
 const addNewContact = 'Novo contato';
 const updateContactList = 'Atualizar lista';
-const saveNewContact = 'Salvar';
-const cancelNewContact = 'Cancelar';
+const saveNewContact = 'SALVAR';
+const cancelNewContact = 'CANCELAR';
 const removeContact = 'Remover';
+
+// About contact
+const nome = 'Nome';
+const apelido = 'Apelido';
+const telefone = 'Telefone';
