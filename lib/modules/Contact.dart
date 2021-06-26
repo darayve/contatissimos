a6c9b7725ffde3984fe568abfb5d6fc69a53912f
Dara Yve <darayve@gmail.com>
2021-06-25 21:56:13 -0300

    remoção de arquivos não utilizados

--- lib/modules/Contact.dart
+++
@@ -1,9 +0,0 @@
-class Contact {
-  // TODO: talvez não precise dessa classe...
-  Contact({this.nome, this.apelido, this.telefone, this.telefoneDdd});
-
-  final String nome;
-  final String apelido;
-  final String telefone;
-  final String telefoneDdd;
-}
